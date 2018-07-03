5838d6f953ab1621d26a74eb36460e1273d3c67b
jasmeet17 <jasmeet17mast@gmail.com>
2018-07-03 01:17:52 -0700

    Add Navbar Component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Navbar';
 
 class App extends Component {
   render() {
     return (
       <div>
          {/* Navigation  */}
-        <nav className="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
-          <div className="container">
-            <a className="navbar-brand" href="index.html">Start Bootstrap</a>
-            <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-              Menu
-              <i className="fa fa-bars"></i>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarResponsive">
-              <ul className="navbar-nav ml-auto">
-                <li className="nav-item">
-                  <a className="nav-link" href="index.html">Home</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="about.html">About</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="post.html">Sample Post</a>
-                </li>
-                <li className="nav-item">
-                  <a className="nav-link" href="contact.html">Contact</a>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </nav>
-
+          <Navbar />
         {/* Page Header */}
         <header className="masthead" styles="background-image: url('img/home-bg.jpg')">
           <div className="overlay"></div>
